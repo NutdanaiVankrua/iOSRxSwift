b5335b4be79f83bb3f85805b2eb458d3db45737c
NutdanaiV Macbook Pro <nutdanai@NutdanaiV-MacBook-Pro.local>
2017-01-24 23:16:51 +0700

    Setup main view controllers

---
+++ iOSRxSwift/Presenters/MainPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MainPresenter.swift
+//  iOSRxSwift
+//
+//  Created by NutdanaiV Macbook Pro on 1/24/2560 BE.
+//  Copyright © 2560 NutdanaiV Macbook Pro. All rights reserved.
+//
+
+import Foundation
+
+class MainPresenter {
+    
+}
---
+++ iOSRxSwift/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  iOSRxSwift
+//
+//  Created by NutdanaiV Macbook Pro on 1/24/2560 BE.
+//  Copyright © 2560 NutdanaiV Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
